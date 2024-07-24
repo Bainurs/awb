9715c2c52f5f3f4bc4135ab2fd11c172fc954a41
shaq32 <nurs.axe@gmail.com>
2024-07-24 18:43:38 +0600

    fix tabPanel

--- src/components/reactComponents/TabPanel.jsx
+++ src/components/reactComponents/TabPanel.jsx
@@ -6,11 +6,11 @@ const TabPanel = ({tabs}) => {
     const [activeTab, setActiveTab] = useState(tabs[0].id);
     return (
         <>
-            <div class="flex flex-wrap flex-row justify-between items-center bg-white fl-menu">
-                <ul class="flex flex-row justify-between items-center bg-white nav-pills">
+            <div className="flex flex-wrap flex-row justify-between items-center bg-white fl-menu">
+                <ul className="flex flex-row justify-between items-center bg-white nav-pills">
                     {
                         tabs.map(item => (
-                                <li class="nav-item relative" key={item.id}>
+                                <li className="nav-item relative" key={item.id}>
                                     <a 
                                         href="#" 
                                         className={`nav-link ${activeTab === item.id ? 'active' : ''}`}
@@ -26,25 +26,25 @@ const TabPanel = ({tabs}) => {
                     }
                 </ul>
             </div>
-            <div class="flex flex-wrap">
-                <div class="tab-content">
+            <div className="flex flex-wrap">
+                <div className="tab-content">
                     {
                         tabs.map(item => 
                         item.id === activeTab ? (
-                            <div class="tap-pane fade active" key={item.id}>
-                                <div class="flex flex-row justify-start items-start fl-content">
-                                    <div class="flex flex-col justify-center items-start fl-col first">
-                                        <img src={item.img.src} alt="" class="fl-col-img flex" />
+                            <div className="tap-pane fade active" key={item.id}>
+                                <div className="flex flex-row justify-start items-start fl-content">
+                                    <div className="flex flex-col justify-center items-start fl-col first">
+                                        <img src={item.img.src} alt="" className="fl-col-img flex" />
                                     </div>
-                                    <div class="flex flex-col justify-center items-start fl-col">
-                                        <div class="flex flex-col justify-center items-start fl-objects">
-                                            <h4 class="fl-title">
+                                    <div className="flex flex-col justify-center items-start fl-col">
+                                        <div className="flex flex-col justify-center items-start fl-objects">
+                                            <h4 className="fl-title">
                                                 {item.title}
                                             </h4>
-                                            <ul class="fl-list">
+                                            <ul className="fl-list">
                                                 {
-                                                    item.list.map(elem => (
-                                                        <li class="relative fl-list-item">
+                                                    item.list.map((elem, index) => (
+                                                        <li className="relative fl-list-item" key={index}>
                                                             {elem}
                                                         </li>
                                                     ))
@@ -55,20 +55,20 @@ const TabPanel = ({tabs}) => {
                                 </div>
                             </div>
                         ) : (
-                            <div class="tap-pane fade" key={item.id}>
-                                <div class="flex flex-row justify-start items-start fl-content">
-                                    <div class="flex flex-col justify-center items-start fl-col first">
-                                        <img src={item.img.src} alt="" class="fl-col-img flex" />
+                            <div className="tap-pane fade" key={item.id}>
+                                <div className="flex flex-row justify-start items-start fl-content">
+                                    <div className="flex flex-col justify-center items-start fl-col first">
+                                        <img src={item.img.src} alt="" className="fl-col-img flex" />
                                     </div>
-                                    <div class="flex flex-col justify-center items-start fl-col">
-                                        <div class="flex flex-col justify-center items-start fl-objects">
-                                            <h4 class="fl-title">
+                                    <div className="flex flex-col justify-center items-start fl-col">
+                                        <div className="flex flex-col justify-center items-start fl-objects">
+                                            <h4 className="fl-title">
                                                 {item.title}
                                             </h4>
-                                            <ul class="fl-list">
+                                            <ul className="fl-list">
                                                 {
                                                     item.list.map((elem, index) => (
-                                                        <li class="relative fl-list-item" key={index}>
+                                                        <li className="relative fl-list-item" key={index}>
                                                             {elem}
                                                         </li>
                                                     ))
@@ -80,8 +80,6 @@ const TabPanel = ({tabs}) => {
                             </div>
                         ))
                     }
-
-                    
                 </div>
             </div>
         </>
